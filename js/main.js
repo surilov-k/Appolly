11060fa403e86e2e653a6fd05007844b6f0c57c9
Kir <surilov51@gmail.com>
2024-10-14 13:22:04 +0300

    Checkout slide desctop

--- js/main.js
+++ js/main.js
@@ -1,11 +1,43 @@
 $(document).ready(function () {
-    $('.slider').slick({
+
+    var rev = $('.slider');
+    rev.on('init', function (event, slick, currentSlide) {
+        var
+            cur = $(slick.$slides[slick.currentSlide]),
+            next = cur.next().next(),
+            prev = cur.prev().prev();
+        prev.addClass('slick-sprev');
+        next.addClass('slick-snext');
+        cur.removeClass('slick-snext').removeClass('slick-sprev');
+        slick.$prev = prev;
+        slick.$next = next;
+    }).on('beforeChange', function (event, slick, currentSlide, nextSlide) {
+        console.log('beforeChange');
+        var
+            cur = $(slick.$slides[nextSlide]);
+        console.log(slick.$prev, slick.$next);
+        slick.$prev.removeClass('slick-sprev');
+        slick.$next.removeClass('slick-snext');
+        next = cur.next().next(),
+            prev = cur.prev().prev();
+        prev.prev();
+        prev.next();
+        prev.addClass('slick-sprev');
+        next.addClass('slick-snext');
+        slick.$prev = prev;
+        slick.$next = next;
+        cur.removeClass('slick-next').removeClass('slick-sprev');
+    });
+
+    rev.slick({
         centerMode: true,
-        centerPadding: '60px',
+        centerPadding: '80px',
         slidesToShow: 3,
         dots: true,
         arrows: true,
         speed: 500,
+        prevArrow: '<button class="slick-prev slick-arrow"><img src="./img/slider/arrow-left-s-line.svg" alt="arrow-left"></button>',
+        nextArrow: '<button class="slick-next slick-arrow"><img src="./img/slider/arrow-left-s-line.svg" alt="arrow-left"></button>',
         responsive: [
             {
                 breakpoint: 768,
@@ -27,6 +59,58 @@ $(document).ready(function () {
             }
         ]
     });
+
+
+    var rev = $('.rev_slider');
+    rev.on('init', function (event, slick, currentSlide) {
+        var
+            cur = $(slick.$slides[slick.currentSlide]),
+            next = cur.next(),
+            prev = cur.prev();
+        prev.addClass('slick-sprev');
+        next.addClass('slick-snext');
+        cur.removeClass('slick-snext').removeClass('slick-sprev');
+        slick.$prev = prev;
+        slick.$next = next;
+    }).on('beforeChange', function (event, slick, currentSlide, nextSlide) {
+        console.log('beforeChange');
+        var
+            cur = $(slick.$slides[nextSlide]);
+        console.log(slick.$prev, slick.$next);
+        slick.$prev.removeClass('slick-sprev');
+        slick.$next.removeClass('slick-snext');
+        next = cur.next(),
+            prev = cur.prev();
+        prev.prev();
+        prev.next();
+        prev.addClass('slick-sprev');
+        next.addClass('slick-snext');
+        slick.$prev = prev;
+        slick.$next = next;
+        cur.removeClass('slick-next').removeClass('slick-sprev');
+    });
+
+    rev.slick({
+        speed: 1000,
+        arrows: true,
+        dots: false,
+        focusOnSelect: true,
+        prevArrow: '<button> prev</button>',
+        nextArrow: '<button> next</button>',
+        infinite: true,
+        centerMode: true,
+        slidesPerRow: 1,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        centerPadding: '0',
+        swipe: true,
+        customPaging: function (slider, i) {
+            return '';
+        },
+        /*infinite: false,*/
+    });
+
+
 });
 
 // documentQuery соседние елементы
